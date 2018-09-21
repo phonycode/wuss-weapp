0d4280d2f23392ea9c2f49aba73380e960290415
cnyballk <584518260@qq.com>
2018-09-21 10:57:00 +0800

    fix: miss addGlobalClass

--- dist/w-avatar/index.js
+++ dist/w-avatar/index.js
@@ -2,10 +2,13 @@
  * @Author: cnyballk[https://github.com/cnyballk] 
  * @Date: 2018-09-10 17:54:29 
  * @Last Modified by: cnyballk[https://github.com/cnyballk]
- * @Last Modified time: 2018-09-19 18:07:28
+ * @Last Modified time: 2018-09-21 10:56:12
  */
 Component({
   externalClasses: ['wuss-class'],
+  options: {
+    addGlobalClass: true,
+  },
   /**
    * 组件的属性列表
    * @param {string} shape 指定头像的形状，可选值为 circle、square
