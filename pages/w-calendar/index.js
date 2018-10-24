2a4cc26fbbfcd9dac2e91fc6e8236dbf5a6bde41
Awei <13650928419@163.com>
2018-10-24 14:58:29 +0800

    Merge branch '2.x' of https://github.com/phonycode/wuss-weapp into 2.x

--- pages/w-calendar/index.js
+++ pages/w-calendar/index.js
@@ -2,13 +2,26 @@
  * @Author: Github.Caitingwei[https://github.com/Caitingwei] 
  * @Date: 2018-09-01 13:25:28 
  * @Last Modified by: cnyballk[https://github.com/cnyballk]
- * @Last Modified time: 2018-09-22 11:52:58
+ * @Last Modified time: 2018-10-24 10:36:25
  */
+const now = new Date();
 Page({
   data: {
     visible1: false,
     visible2: false,
     visible3: false,
+    defaultValue: [new Date().getTime(), '2018-12-1'],
+    disabledDate: {
+      '2018-10-29': {
+        text: '禁用',
+      },
+      '2018-10-30': {
+        text: '你点不到我',
+      },
+      '2018-10-31': {
+        text: '禁用',
+      },
+    },
   },
   handleClick1() {
     this.setData({ visible1: !this.data.visible1 });
@@ -20,6 +33,8 @@ Page({
     this.setData({ visible3: !this.data.visible3 });
   },
   handleCancel() {
+    console.log('cancel');
+
     this.setData({
       visible1: false,
       visible2: false,
@@ -28,12 +43,14 @@ Page({
   },
   handleSelected(date) {
     const {
-      detail: { checkInDate, checkOutDate, checkTotalDay },
+      detail: { value },
     } = date;
-    wx.showModal({
-      title: '提示',
-      content: `您选择了 入住日期:${checkInDate}，离店日期:${checkOutDate},总天数:${checkTotalDay}`,
-      showCancel: false,
-    });
+    console.log(value);
+
+    // wx.showModal({
+    //   title: '提示',
+    //   content: `您选择了 入住日期:${checkInDate}，离店日期:${checkOutDate},总天数:${checkTotalDay}`,
+    //   showCancel: false,
+    // });
   },
 });
