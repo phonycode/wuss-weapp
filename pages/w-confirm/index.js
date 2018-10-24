07c46e3931123b9dfd91cfee5584e9f47c7927b3
Awei <13650928419@163.com>
2018-10-24 09:05:56 +0800

    feat: Added confirm component.

---
+++ pages/w-confirm/index.js
@@ -0,0 +1,39 @@
+import { Confirm } from '../../dist/index';
+
+Page({
+  handleClick1() {
+    Confirm({
+      title: '警告',
+      content: '你确定要删除吗？',
+      confirm: () => { console.log('confirm') },
+      cancel: () => { console.log('cancel') },
+    })
+  },
+  handleClick2() {
+    Confirm({
+      title: '你变了',
+      content: '你变绿了',
+      cancelTextColor: '#55b2f0',
+      confirmTextColor: 'green',
+    })
+  },
+  handleClick3() {
+    Confirm({
+      title: '提示',
+      content: '你爱我吗？',
+      cancelText: '忘了爱',
+      confirmText: '爱',
+      cancelTextColor: '#999',
+      confirmTextColor: 'red',
+      confirm: () => { console.log('confirm') },
+      cancel: () => { console.log('cancel') },
+    })
+  },
+  handleClick4() {
+    Confirm({
+      title: '提示',
+      showCancel: false,
+      content: '你只能点确定',
+    })
+  },
+});
