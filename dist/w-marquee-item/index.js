4ec325295e0eb27bd41f15e804b621e859618e88
Awei <13650928419@163.com>
2018-10-24 09:04:57 +0800

    feat: Added marquee component.

---
+++ dist/w-marquee-item/index.js
@@ -0,0 +1,75 @@
+import Behavior from '../common/behavior/index';
+
+const Marquee = '../w-marquee/index';
+
+Component({
+   /**
+   * 继承父组件的class
+   */
+  externalClasses: ['wuss-class'], 
+  
+  /**
+   * 组件间关系定义
+   */
+   relations: {
+    [Marquee]: {
+      type: 'ancestor',
+    },
+   },
+   
+  /**
+   * 组件选项
+   */
+   options: {
+   },
+   
+  /**
+   * 组件间关系定义
+   */
+  behaviors: [Behavior],
+   
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
+  
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    height: 0,
+  },
+  
+  /**
+   * 组件方法列表
+   */
+  methods: {
+    handleClick(e) {
+      this.triggerEvent('onClick',e)
+    },
+    updateItemHeight(height) {
+      if(height) this.setData({ height })
+    }
+  },
+  
+  /**
+   * 在组件实例进入页面节点树时执行
+   */
+  created: function() {},
+   
+  /**
+   * 组件布局完成后执行
+   */
+  ready: function() {},
+  
+  /**
+   * 在组件实例进入页面节点树时执行
+   */
+   attached: function() {},
+   
+  /**
+   * 在组件实例被移动到节点树另一个位置时执行
+   */
+   moved: function() {},
+   
+})
