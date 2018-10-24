626c572ab71e14cc3ca77eff306998c0fe1dc799
Awei <13650928419@163.com>
2018-10-24 09:05:37 +0800

    feat: Added number component.

---
+++ dist/w-number/index.js
@@ -0,0 +1,169 @@
+import Behavior from '../common/behavior/index';
+
+Component({
+  /**
+   * 继承父组件的class
+   */
+  externalClasses: ['wuss-class'],
+
+  /**
+   * 组件间关系定义
+   */
+  relations: {},
+
+  /**
+   * 组件选项
+   */
+  options: {},
+
+  /**
+   * 组件间关系定义
+   */
+  behaviors: [Behavior],
+
+  /**
+   * 组件的属性列表
+   * @param defaultValue 当前默认值
+   * @param max 最大值
+   * @param min 最小值
+   * @param width 当前输入框宽度
+   * @param steps 步进数
+   * @param fillable 是否可填写
+   * @param buttonType 按钮样式，可选[box/circel]
+   * @param disabled 禁用
+   */
+  properties: {
+    defaultValue: {
+      type: Number,
+      value: 0,
+    },
+    max: {
+      type: Number,
+      value: Infinity,
+    },
+    width: {
+      type: Number,
+      value: 35,
+    },
+    min: {
+      type: Number,
+      value: 0,
+    },
+    steps: {
+      type: Number,
+      value: 1,
+    },
+    fillable: {
+      type: Boolean,
+      value: true,
+    },
+    buttonType: {
+      type: String,
+      value: 'box',
+    },
+    disabled: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    _cutDisabled: false,
+    _addDisabled: false,
+    value: 0,
+  },
+
+  /**
+   * 组件方法列表
+   */
+  methods: {
+    handleInput(e) {
+      let value = Number(e.detail.value);
+      this.computed(value)
+    },
+    computed(value) {
+      let {
+        _cutDisabled,
+        _addDisabled,
+        max,
+        min
+      } = this.data;
+      if (typeof value !== 'number' || isNaN(value) || value <= min) {
+        value = min;
+        _cutDisabled = true;
+        _addDisabled = false;
+      } else if (value >= max) {
+        value = max;
+        _cutDisabled = false;
+        _addDisabled = true;
+      } else {
+        _cutDisabled = false;
+        _addDisabled = false;
+      }
+      this.setData({
+        value,
+        _cutDisabled,
+        _addDisabled,
+      })
+    },
+    handleBlur(e) {
+      const { value } = this.data;
+      this.triggerEvent('onChange',{ value },{});
+    },
+    handleConfirm(e) {
+      const { value } = this.data;
+      this.triggerEvent('onChange',{ value },{});
+    },
+    handleCut() {
+      let {
+        value,
+        steps,
+        disabled,
+      } = this.data;
+      if (this.data._cutDisabled || disabled) return false;
+      this.computed(value -= steps);
+      this.triggerEvent('onChange',{ value },{});
+    },
+    handleAdd() {
+      let {
+        value,
+        steps,
+        disabled,
+      } = this.data;
+      if (this.data._addDisabled || disabled) return false;
+      this.computed(value += steps);
+      this.triggerEvent('onChange',{ value },{});
+    },
+  },
+
+  /**
+   * 在组件实例进入页面节点树时执行
+   */
+  created: function () {},
+
+  /**
+   * 组件布局完成后执行
+   */
+  ready: function () {
+    const {
+      defaultValue,
+      min
+    } = this.data;
+    if(min<0) throw Error('min 必须大于或等于0');
+    this.computed(defaultValue)
+  },
+
+  /**
+   * 在组件实例进入页面节点树时执行
+   */
+  attached: function () {},
+
+  /**
+   * 在组件实例被移动到节点树另一个位置时执行
+   */
+  moved: function () {},
+
+})
---
+++ pages/w-number/index.js
@@ -0,0 +1,66 @@
+// pages/w-number/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: ['娃娃菜', "牛肉脯", "鸡柳", "酸菜鱼", "卤蛋", "鱼豆腐", "章鱼丸", "空心菜", "猪血", "番茄", "黄瓜", "鸭肠", "牛杂"],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
