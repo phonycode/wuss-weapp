43bedbac5ab632ea8de4a49cbc4f7778300afc17
Awei <13650928419@163.com>
2018-12-25 15:09:06 +0800

    docs: update example page.

--- pages/w-scrollable-tab-view/example1.js
+++ pages/w-scrollable-tab-view/example1.js
@@ -17,11 +17,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {},
-  handleScrollableChange(e) {
-    this.setData({
-      currentIndex: e.detail.value,
-    })
-  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
